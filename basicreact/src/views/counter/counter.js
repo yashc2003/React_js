1821cfe660d81509c99104b3a1d93bcb00c6357a
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2023-10-06 08:37:53 +0000

    update file

---
+++ basicreact/src/views/counter/counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+function counter() {
+
+    const [count, setCount] = useState(0);
+
+    return (
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+    );
+  }
+  
+  export default counter;
+  
